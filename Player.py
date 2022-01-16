4ffe3be6453cf52b97e83de3b9bd040902239a08
mikimb99 <65571317+mikimb99@users.noreply.github.com>
2022-01-16 13:33:36 +0100

    Test 2 mal

--- Player.py
+++ Player.py
@@ -2,13 +2,5 @@ import unittest
 
 class Player(unittest.TestCase):
 
-    score=["0","15","30","40"] #definidos los puntos
-
-    def __init__(self): #definimos el marcador inicial
-        self.score = "0"
-
-    def ganarPunto(self): #definimos el 15
-        self.score= self.score+1
-    def getScore(self):
-        return self.score[self.score]
-
+    def __init__(self):
+        pass
--- Test.py
+++ Test.py
@@ -1,20 +1,13 @@
 import unittest
 
 import Player
-from Player import Player
+from Player import *
 
 
 class Test(unittest.TestCase):
 
     def test_inicial(self):  # primero defino al jugador y puesto a 0 el marcador
         player = Player()
-        self.assertEquals("0", player.getScore())
-    def test_jugador15(self):
-        player= Player()
-        player.ganarPunto()
-        self.assertEquals("15",player.getScore())
-    def test_jugador30(self):
-        player= Player()
-        player.ganarPunto()
-        player.ganarPunto()
-        self.assertEquals("30", player.getScore())
+    def test_0_0(self):
+        player = Player()
+        self.assertEquals("0-0",player.calcularPuntuacion())
