d661c3e322fce621790fca0712f6fe61a3bc619f
mikimb99 <65571317+mikimb99@users.noreply.github.com>
2022-01-16 12:30:39 +0100

    Test 2 mal

--- Player.py
+++ Player.py
@@ -2,7 +2,7 @@ import unittest
 
 class Player(unittest.TestCase):
 
-    score=["0","15","30","40","A","W"]
+    score=["0","15","30","40","A","W"] #definido el juego
 
     def getScore(self):
         return "0"
--- Test.py
+++ Test.py
@@ -4,7 +4,9 @@ from Player import Player
 
 class Test(unittest.TestCase):
 
-     def test_inicial(self): #primero defino al jugador
-         player= Player()
-         self.assertEquals("0", player.getScore())
-
+    def test_inicial(self):  # primero defino al jugador y puesto a 0 el marcador
+        player = Player()
+        self.assertEquals("0", player.getScore())
+    def test_jugador15(self):
+        player= Player()
+        self.assertEquals("15",player.getScore())
