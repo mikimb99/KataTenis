c3a0f51521f000dd1ab659e7f92e251646075538
mikimb99 <65571317+mikimb99@users.noreply.github.com>
2022-01-16 13:37:32 +0100

    test 15_0 mal

--- Test.py
+++ Test.py
@@ -11,3 +11,7 @@ class Test(unittest.TestCase):
     def test_0_0(self):
         player = Player()
         self.assertEquals("0-0",player.calcularPuntuacion())
+    def test_15_0(self):
+        player = Player()
+        player.autoincrementarJug1()
+        self.assertEquals("15-0",player.calcularPuntuacion())
